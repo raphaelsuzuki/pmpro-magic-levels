39094ca4e9f5d87cf1800f4a4948cb1413430d2c
Raphael Suzuki <szkds@tuta.io>
2026-03-15 20:01:48 +0900

    Fix CI/review blockers and harden workflow

--- tests/CacheKeyNormalizationTest.php
+++ tests/CacheKeyNormalizationTest.php
@@ -2,66 +2,64 @@
 
 use PHPUnit\Framework\TestCase;
 
-class CacheKeyNormalizationTest extends TestCase
-{
-	public function test_generate_key_normalizes_string_fields()
-	{
+class CacheKeyNormalizationTest extends TestCase {
+
+	public function test_generate_key_normalizes_string_fields() {
 		$raw_params = array(
-			'name' => "  <b>Group - Gold</b>  ",
-			'description' => "  <em>Premium tier</em>\n",
-			'cycle_period' => " Month\t",
-			'expiration_period' => " Year ",
-			'billing_amount' => '29.99',
-			'cycle_number' => 1,
-			'initial_payment' => 0,
-			'trial_amount' => 0,
-			'trial_limit' => 0,
-			'billing_limit' => 0,
+			'name'              => '  <b>Group - Gold</b>  ',
+			'description'       => "  <em>Premium tier</em>\n",
+			'cycle_period'      => " Month\t",
+			'expiration_period' => ' Year ',
+			'billing_amount'    => '29.99',
+			'cycle_number'      => 1,
+			'initial_payment'   => 0,
+			'trial_amount'      => 0,
+			'trial_limit'       => 0,
+			'billing_limit'     => 0,
 			'expiration_number' => 0,
 		);
 
 		$normalized_params = array(
-			'name' => sanitize_text_field($raw_params['name']),
-			'description' => sanitize_textarea_field($raw_params['description']),
-			'cycle_period' => sanitize_text_field($raw_params['cycle_period']),
-			'expiration_period' => sanitize_text_field($raw_params['expiration_period']),
-			'billing_amount' => '29.99',
-			'cycle_number' => 1,
-			'initial_payment' => 0,
-			'trial_amount' => 0,
-			'trial_limit' => 0,
-			'billing_limit' => 0,
+			'name'              => sanitize_text_field( $raw_params['name'] ),
+			'description'       => sanitize_textarea_field( $raw_params['description'] ),
+			'cycle_period'      => sanitize_text_field( $raw_params['cycle_period'] ),
+			'expiration_period' => sanitize_text_field( $raw_params['expiration_period'] ),
+			'billing_amount'    => '29.99',
+			'cycle_number'      => 1,
+			'initial_payment'   => 0,
+			'trial_amount'      => 0,
+			'trial_limit'       => 0,
+			'billing_limit'     => 0,
 			'expiration_number' => 0,
 		);
 
 		$this->assertSame(
-			PMPRO_Magic_Levels_Cache::generate_key($normalized_params),
-			PMPRO_Magic_Levels_Cache::generate_key($raw_params)
+			PMPRO_Magic_Levels_Cache::generate_key( $normalized_params ),
+			PMPRO_Magic_Levels_Cache::generate_key( $raw_params )
 		);
 	}
 
-	public function test_generate_key_changes_when_description_changes()
-	{
+	public function test_generate_key_changes_when_description_changes() {
 		$base = array(
-			'name' => 'Group - Gold',
-			'description' => 'Description A',
-			'billing_amount' => 29.99,
-			'cycle_number' => 1,
-			'cycle_period' => 'Month',
-			'initial_payment' => 0,
-			'trial_amount' => 0,
-			'trial_limit' => 0,
-			'billing_limit' => 0,
+			'name'              => 'Group - Gold',
+			'description'       => 'Description A',
+			'billing_amount'    => 29.99,
+			'cycle_number'      => 1,
+			'cycle_period'      => 'Month',
+			'initial_payment'   => 0,
+			'trial_amount'      => 0,
+			'trial_limit'       => 0,
+			'billing_limit'     => 0,
 			'expiration_number' => 0,
 			'expiration_period' => '',
 		);
 
-		$changed = $base;
+		$changed                = $base;
 		$changed['description'] = 'Description B';
 
 		$this->assertNotSame(
-			PMPRO_Magic_Levels_Cache::generate_key($base),
-			PMPRO_Magic_Levels_Cache::generate_key($changed)
+			PMPRO_Magic_Levels_Cache::generate_key( $base ),
+			PMPRO_Magic_Levels_Cache::generate_key( $changed )
 		);
 	}
 }
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,32 +1,33 @@
 <?php
 
-defined('ABSPATH') || define('ABSPATH', __DIR__ . '/');
+// phpcs:disable WordPress.WP.AlternativeFunctions
 
-if (!function_exists('sanitize_text_field')) {
-	function sanitize_text_field($value)
-	{
-		$value = is_scalar($value) ? (string) $value : '';
-		$value = strip_tags($value);
-		$value = preg_replace('/[\r\n\t ]+/', ' ', $value);
-		return trim($value);
+defined( 'ABSPATH' ) || define( 'ABSPATH', __DIR__ . '/' );
+
+if ( ! function_exists( 'sanitize_text_field' ) ) {
+	function sanitize_text_field( $value ) {
+		$value = is_scalar( $value ) ? (string) $value : '';
+		$value = strip_tags( $value );
+		$value = preg_replace( '/[\r\n\t ]+/', ' ', $value );
+		return trim( $value );
 	}
 }
 
-if (!function_exists('sanitize_textarea_field')) {
-	function sanitize_textarea_field($value)
-	{
-		$value = is_scalar($value) ? (string) $value : '';
-		$value = strip_tags($value);
-		$value = preg_replace('/\r\n|\r/', "\n", $value);
-		return trim($value);
+if ( ! function_exists( 'sanitize_textarea_field' ) ) {
+	function sanitize_textarea_field( $value ) {
+		$value = is_scalar( $value ) ? (string) $value : '';
+		$value = strip_tags( $value );
+		$value = preg_replace( '/\r\n|\r/', "\n", $value );
+		return trim( $value );
 	}
 }
 
-if (!function_exists('wp_json_encode')) {
-	function wp_json_encode($value)
-	{
-		return json_encode($value);
+if ( ! function_exists( 'wp_json_encode' ) ) {
+	function wp_json_encode( $value ) {
+		return json_encode( $value );
 	}
 }
 
-require_once dirname(__DIR__) . '/includes/class-cache.php';
+require_once dirname( __DIR__ ) . '/includes/class-cache.php';
+
+// phpcs:enable WordPress.WP.AlternativeFunctions
